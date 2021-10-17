b5c4e40a777a832639b5316d4f83cdf79d1aaa27
Jackson Visser <jvisser@pugetsound.edu>
2021-10-17 16:12:57 -0700

    grabbed nodes successfully

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//grab nodes
+let addyNode = document.querySelector("#address");
+console.dir(addyNode);
+
+let timeNode = document.querySelector("#time");
+console.dir(timeNode);
+
+let byobNode = document.querySelector("#byob-options");
+console.dir(byobNode);
+
+let vibeNode = document.querySelector("#vibe-options");
+console.dir(vibeNode);
+
+let inviteNode = document.querySelector("#invite-options");
+console.dir(inviteNode);
+
+let previewNode = document.querySelector("#preview");
+console.dir(previewNode);
